ef0ab4df4fbbde9e9c5a3662565f867e4078b23b
imon-n <imon.eeecu@gmail.com>
2026-06-05 17:18:00 +0600

    ad page

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,16 +1,19 @@
-import { CheckoutPageClient } from "./CheckoutPageClient";
+import CheckoutPageClient from "./CheckoutPageClient";
+
 const backendUrl =
-  process.env.NEXT_PUBLIC_BACKEND_URL ||
   process.env.NEXT_PUBLIC_API_URL ||
   process.env.BACKEND_URL ||
-  "http://localhost:5001";
+  "http://localhost:5000";
+
 const frontendUrl =
-  process.env.NEXT_PUBLIC_FRONTEND_URL ||
   process.env.FRONTEND_URL ||
   "https://assignment5-frontend-seven.vercel.app";
 
 export default function CheckoutPage() {
   return (
-    <CheckoutPageClient backendUrl={backendUrl} frontendUrl={frontendUrl} />
+    <CheckoutPageClient
+      backendUrl={backendUrl}
+      frontendUrl={frontendUrl}
+    />
   );
 }
