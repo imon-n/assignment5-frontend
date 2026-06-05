cb035424250e7291d0327c876fadb44631cad11d
imon-n <imon.eeecu@gmail.com>
2026-06-05 17:04:12 +0600

    ad

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -9,7 +9,7 @@ import { createAuthClient } from "better-auth/react"; // make sure to import fro
 
 export const authClient = createAuthClient({
   //you can pass client configuration here
-  baseURL: "https://assignment5-backend-f7q4.onrender.com/api/auth",
+  baseURL: "https://assignment5-backend-f7q4.onrender.com",
   fetchOptions: { credentials: "include" },
 
   plugins: [
