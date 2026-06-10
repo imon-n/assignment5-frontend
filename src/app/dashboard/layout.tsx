62a560328a099a466467a75e1bd9ec72acca5669
imon-n <imon.eeecu@gmail.com>
2026-06-10 11:02:48 +0600

    added all now

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 type Role = "STUDENT" | "TUTOR" | "ADMIN";
 
@@ -24,29 +25,78 @@ export default function DashboardLayout({
     process.env.NEXT_PUBLIC_API_URL ||
     "https://assignment5-backend-f7q4.onrender.com";
 
-  useEffect(() => {
+  // useEffect(() => {
+  //   const getMe = async () => {
+  //     try {
+  //       const res = await fetch(`${API_URL}/api/me`, {
+  //         method: "GET",
+  //         credentials: "include", // 🔥 MUST
+  //       });
+
+  //       console.log("STATUS:", res.status);
+
+  //       if (!res.ok) {
+  //         throw new Error("Unauthorized");
+  //       }
+
+  //       const data = await res.json();
+  //       console.log("USER:", data);
+
+  //       setUser(data.user || data.data || data);
+  //     } catch (error) {
+  //       console.error("Session error:", error);
+  //       setUser(null);
+  //       router.replace("/login");
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+
+  //   getMe();
+  // }, [API_URL, router]);
+
+    useEffect(() => {
     const getMe = async () => {
-      try {
-        const res = await fetch(`${API_URL}/api/me`, {
-          method: "GET",
-          credentials: "include", // 🔥 MUST
-        });
+        try {
 
-        console.log("STATUS:", res.status);
+    const sessionRes = await fetch(
+      `${API_URL}/api/auth/get-session`,
+      {
+        method: "GET",
+        credentials: "include",
+      }
+    );
 
-        if (!res.ok) {
-          throw new Error("Unauthorized");
-        }
+    if (!sessionRes.ok) {
+      toast.error("Session not found");
+      return;
+    }
 
-        const data = await res.json();
-        console.log("USER:", data);
+    const sessionData = await sessionRes.json();
 
-        setUser(data.user || data.data || data);
-      } catch (error) {
-        console.error("Session error:", error);
-        setUser(null);
-        router.replace("/login");
-      } finally {
+    console.log("SESSION1:", sessionData);
+
+    if (!sessionData?.user) {
+      toast.error("User not found");
+      return;
+    }
+
+    toast.success("Login successful");
+
+    const role = sessionData.user.role;
+
+    if (role === "ADMIN") {
+      router.replace("/admin");
+    } else if (role === "TUTOR") {
+      router.replace("/tutors");
+    } else {
+      router.replace("/dashboard");
+    }
+  } catch (error) {
+    console.error(error);
+    toast.error("Something went wrong");
+  }
+     finally {
         setLoading(false);
       }
     };
