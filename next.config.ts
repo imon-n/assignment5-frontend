3c729227f214271792e0ce42d728e11f60921914
imon-n <imon.eeecu@gmail.com>
2026-06-10 13:20:21 +0600

    change confic here

--- next.config.ts
+++ next.config.ts
@@ -36,6 +36,23 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+   reactCompiler: true,
+
+  // better-auth proxy
+  async rewrites() {
+    return [
+      {
+        // Explicitly map auth requests
+        source: "/api/auth/:path*",
+        destination: process.env.NEXT_PUBLIC_BACKEND_URL + "/api/auth/:path*",
+      },
+      {
+        // Explicitly map v1 API requests
+        source: "/api/:path*",
+        destination: process.env.NEXT_PUBLIC_BACKEND_URL + "/api/v1/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
