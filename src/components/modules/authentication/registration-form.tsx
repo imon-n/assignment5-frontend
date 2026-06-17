080159ce971fbd5e7c2acc2146a613be6abc4e05
imon-n <imon.eeecu@gmail.com>
2026-06-17 11:05:30 +0600

    added all

--- src/components/modules/authentication/registration-form.tsx
+++ src/components/modules/authentication/registration-form.tsx
@@ -50,7 +50,7 @@ export function RegisterForm() {
     name: "",
     email: "",
     password: "",
-    role: "student",
+    role: "STUDENT",
     image: "",
   });
 
@@ -142,7 +142,7 @@ export function RegisterForm() {
         />
 
         <Input
-          placeholder="Role (student/tutor)"
+          placeholder="Role (STUDENT/TUTOR)"
           onChange={(e) => setForm({ ...form, role: e.target.value })}
         />
 
